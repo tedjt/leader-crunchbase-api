10a29d6758afc8208f2a1cf5bd43676fb75d5606
ivolo <ivolo@mit.edu>
2014-02-03 16:33:53 -0800

    fixing company creation bug and looser domain selection

--- index.js
+++ index.js
@@ -48,18 +48,26 @@ function middleware (apiKey) {
  */
 
 function details (profile, person) {
-  var company = person.company;
-  extend(true, company, map(profile, {
+  person.company = person.company || {};
+  extend(true, person.company, map(profile, {
     'tags': 'tag_list',
     'employees': 'number_of_employees',
     'category': 'category_code',
     'crunchbase.url': 'crunchbase_url',
-    'funding': 'total_money_raised'
+    'funding': 'total_money_raised',
+    'twitter.username': 'twitter_username'
   }));
 
   var crunchimage = profile.image && profile.image.available_sizes;
   if (crunchimage && crunchimage.length && crunchimage[0].length > 1) {
     company.image = 'http://www.crunchbase.com/' + crunchimage[0][1];
+  }
+
+  if (profile.external_links) {
+    person.company.links = person.company.links || [];
+    profile.external_links.forEach(function (item) {
+      person.company.links.push({ title: item.title, link: item.external_url });
+    });
   }
 }
 
@@ -111,7 +119,7 @@ function getCompanyName (person, context) {
  */
 
 function getInterestingDomain (person, context) {
-  if (person.domain && person.domain.interesting)
+  if (person.domain && !person.domain.disposable && !person.domain.personal)
     return person.domain.name;
   else
     return null;
